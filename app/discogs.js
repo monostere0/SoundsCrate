16bac0060de024eb66695897f81ba23cbc7b7f23
Daniel Leu <leudanielm@gmail.com>
2017-04-02 03:26:16 +0200

    Remove duplicates from the mock data

--- app/discogs.js
+++ app/discogs.js
@@ -9,10 +9,14 @@ const records = [
   'https://img.discogs.com/nYEYVK_WnqBpf-NUOvHc4MceKfQ=/fit-in/480x480/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-7577132-1444405711-8181.jpeg.jpg',
   'https://img.discogs.com/7O6yIBv2gMx_IoK_7I0ooFFCU3k=/fit-in/600x598/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-1097885-1425019145-2733.jpeg.jpg',
   'https://img.discogs.com/5niM71_Gc3Zkk6VK4cRlpHpQRIY=/fit-in/600x590/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-499497-1456047383-9489.jpeg.jpg',
-  'https://img.discogs.com/7O6yIBv2gMx_IoK_7I0ooFFCU3k=/fit-in/600x598/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-1097885-1425019145-2733.jpeg.jpg',
-  'https://img.discogs.com/5niM71_Gc3Zkk6VK4cRlpHpQRIY=/fit-in/600x590/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-499497-1456047383-9489.jpeg.jpg',
+  'https://img.discogs.com/InJ-Rqs9mSutYI6MrEk3P60aVis=/fit-in/450x450/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2607734-1292931358.jpeg.jpg',
+  'https://img.discogs.com/e1GPjWk3N7aYx01q7tMZnwtE6vI=/fit-in/500x500/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2900961-1306377993.jpeg.jpg',
+  'https://img.discogs.com/LKRTQ0R4qcnzR5I5bhLkTKt2XPI=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-1174296-1363434073-2265.jpeg.jpg',
+  'https://img.discogs.com/cIJvyuE5Hwssqu6vSn0x7xnN-0A=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2920039-1424390415-9097.jpeg.jpg',
+  'https://img.discogs.com/0NeaTq5-Cmmtr4gqyN-zIlJ6TuI=/fit-in/299x300/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-7618170-1445261287-7218.jpeg.jpg',
+  'https://img.discogs.com/4kGbgmvIuVDwCqTDY6-8Be_kpO8=/fit-in/450x449/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-5032062-1382639782-6810.jpeg.jpg',
 ];
 
 export function getRecords() {
-  return Promise.resolve(records.concat(records));
+  return Promise.resolve(records);
 }

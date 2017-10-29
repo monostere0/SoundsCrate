b5fa0fe96bd3fe9df329287f4068fd6118ddf836
Daniel Leu <leudanielm@gmail.com>
2017-10-29 20:49:45 +0100

    Upgrade to react native 0.49.5 (#9)

--- ios/SoundsCrate/AppDelegate.m
+++ ios/SoundsCrate/AppDelegate.m
@@ -18,9 +18,9 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   NSURL *jsCodeLocation;
-  
+
 #ifdef DEBUG
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
+  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 #else
   jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
